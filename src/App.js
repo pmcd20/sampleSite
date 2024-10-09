3ace3f2dc8ff6818574e4858e9bad60a884dbe3f
pmcd20 <pmcd20@gmai.com>
2024-10-09 21:21:15 +0100

    Added React Nav Bar

--- src/App.js
+++ src/App.js
@@ -1,9 +1,15 @@
+// App.js
 import React from 'react';
+import Nav from './components/nav'; // Import Nav component
+import './index.css';
 
 function App() {
   return (
-    <div className="text-center">
-      Hello World 2024
+    <div className="App">
+       <Nav /> 
+      <div className="text-center bg-red-50" style={{ color: 'green' }}>
+        Nav Bar
+      </div>
     </div>
   );
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,12 @@
-/** @type {import('tailwindcss').Config} */
+// tailwind.config.js
 module.exports = {
   content: [
-    "./src/**/*.{js,jsx,ts,tsx}",
+    './src/**/*.{js,jsx,ts,tsx}',  // Ensure it includes JSX and TSX files
   ],
   theme: {
     extend: {},
   },
   plugins: [],
-}
+};
 
 
